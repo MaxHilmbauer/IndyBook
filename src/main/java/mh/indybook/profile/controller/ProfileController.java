6225eaef21a1cd53b1420ef17954243b6efab47d
mxh <maximilian.hilmbauer@outlook.com>
2023-03-26 00:28:55 +0100

    Removed unnecessary comment

--- src/main/java/mh/indybook/profile/controller/ProfileController.java
+++ src/main/java/mh/indybook/profile/controller/ProfileController.java
@@ -24,7 +24,6 @@ public class ProfileController {
                 while ((line = sc.nextLine()).contains(" - ")) {
                     String teacher = line.split(" - ")[1];
                     //profile.getTeachers().add(teacher);
-                    // Test
                 }
             }
             if (line.contains("Subject:")) {
